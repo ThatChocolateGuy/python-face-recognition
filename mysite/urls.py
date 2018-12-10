18f636b84bc63b5539303e359e2dec23ef5d90cc
Nem <nemx14@gmail.com>
2018-12-09 23:02:02 -0500

    reverted urls.py mod

--- mysite/urls.py
+++ mysite/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     url(r'^photos/', include('mysite.photos.urls', namespace='photos')),
 ]
 
-# if settings.DEBUG:
-urlpatterns += static(
+if settings.DEBUG:
+    urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
