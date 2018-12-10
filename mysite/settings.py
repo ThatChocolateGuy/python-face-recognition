8eab25cacd3f0db508f10acd1d06b26622fd842d
Nem <nemx14@gmail.com>
2018-12-09 23:41:36 -0500

    got rid of django-heroku - app django version < 2.0

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,15 +128,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'mysite/static')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'mysite/photos/static'),
-)
-
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR,  'templates'),
-    # Add to this list all the locations containing your static files
-    os.path.join(BASE_DIR,  'photos/templates'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'mysite/photos/static'),
+# )
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
@@ -145,9 +138,6 @@ TEMPLATE_DIRS = (
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
 
 LOGGING = {
     'version': 1,
