5bc876307eb527f85869557d530b5c19535ddbf0
Nem <nemx14@gmail.com>
2020-02-12 15:09:50 -0500

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,7 +6,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
-    url(r'^photos/', include('mysite.photos.urls', namespace='photos')),
+    url(r'^photos/', include(('mysite.photos.urls', 'mysite'), namespace='photos')),
 ]
 
 if settings.DEBUG:
