242520ca89ea2f49016a4b1a6f5529a3ff009b57
Nem <nemx14@gmail.com>
2020-02-12 16:13:28 -0500

    remove urlpatterns append on debug

--- mysite/urls.py
+++ mysite/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     url(r'^photos/', include(('mysite.photos.urls', 'mysite'), namespace='photos')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+urlpatterns += static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
