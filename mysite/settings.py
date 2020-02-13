10d570a72a1fc50a9d2679f523d57931478d66b6
Nem <nemx14@gmail.com>
2020-02-12 23:08:21 -0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -172,3 +172,7 @@ LOGGING = {
     }
 }
 
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
