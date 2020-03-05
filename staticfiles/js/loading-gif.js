af5ada66cc76636ed86da8145e245bc4d3b4b255
thatchocolateguy <nemx14@gmail.com>
2020-03-05 13:14:37 -0500

    add staticfiles

---
+++ staticfiles/js/loading-gif.js
@@ -0,0 +1,9 @@
+$(".loading-gif").click(function () {
+  $(".se-pre-con").show();
+});
+
+// // Wait for window load
+// $(window).load(function () {
+//   // Animate loader off screen
+//   $(".se-pre-con").fadeOut("slow");
+// });
