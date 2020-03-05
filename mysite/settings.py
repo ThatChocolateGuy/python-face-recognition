637288d88c8b158eb02cac6a3fe452d8dcc11cb5
thatchocolateguy <nemx14@gmail.com>
2020-03-05 16:27:18 -0500

    add p-f-r.herokuapp.com to allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,8 @@ DEBUG = env.bool('DJANGO_DEBUG', default=False)
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    'python-face-recognition.herokuapp.com'
+    'python-face-recognition.herokuapp.com',
+    'p-f-r.herokuapp.com/'
 ]
 
 
